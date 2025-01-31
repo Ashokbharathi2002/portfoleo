7a256ca2e937f232399e45d094cbb67631663a45
ashok bharathi <bharathi17barathi@gmail.com>
2025-01-31 17:15:16 +0530

    adding a form

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,20 @@ function msddata() {
   console.log("Name: " + name + "\nEmail: " + email + "\nsubject: " + subject + "\nMessage: " + message);
 }
  
- var form = document.getElementById('sheetdb-form');
-  form.addEventListener("submit", e => {
-    e.preventDefault();
-    fetch(form.action, {
-        method : "POST",
-        body: new FormData(document.getElementById("sheetdb-form")),
-    }).then(
-        response => response.json()
-    ).then((html) => {
-        document.getElementById("sucmsg").style.display="block";
-        document.getElementById("msgform").style.display="none";
-        form.reset()
-
-    });
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+    method : "POST",
+    body: new FormData(document.getElementById("sheetdb-form")),
+  }).then(
+    response => response.json()
+  ).then((html) => {
+    document.getElementById("sucmsg").style.display="block";
+    document.getElementById("msgform").style.display="none";
+    form.reset()
   });
+});
 // dark mode
 function myFunction() {
   var element = document.body;
